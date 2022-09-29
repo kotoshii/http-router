5b1c9154573c36f9a4212c9cad19dd945d808301
Oleksandr Semeniuk <o.semeniuk31@gmail.com>
2022-09-29 14:44:40 +0300

    remove semicolons, eslint fixes

--- src/types/MiddlewareFunction.ts
+++ src/types/MiddlewareFunction.ts
@@ -1,6 +1,6 @@
-import { Request } from './Request';
-import { Response } from './Response';
+import { Request } from './Request'
+import { Response } from './Response'
 
 // TODO find a way to change unknown to something more specific
 // TODO change next function type signature
-export type MiddlewareFunction = (req?: Request, res?: Response, next?: () => void) => unknown;
+export type MiddlewareFunction = (req?: Request, res?: Response, next?: () => void) => unknown
