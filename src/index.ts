517395fd0df2f69905f34dcfa99b1436488b9812
misaki <>
2022-10-02 19:22:57 +0300

    expose additional entities to user

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata'
 import { Server } from './server/Server/Server'
 
-export * from './decorators/Controller'
+export { Controller } from './decorators/Controller'
 export * from './decorators/routes'
+export { MiddlewareFunction } from './types/MiddlewareFunction'
+export { Request } from './types/Request'
+export { Response } from './types/Response'
+export { HttpMethods } from './constants/httpMethods'
 
 export function createServer() {
   return new Server()
